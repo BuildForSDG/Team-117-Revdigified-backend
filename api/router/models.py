96cf3341cb30964cb0f8548c51f31d4e4e2691a3
Samizzo <samizzographics@gmail.com>
2020-05-23 11:32:10 +0300

    users

--- api/router/models.py
+++ api/router/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    is_taxpayer = models.BooleanField(default=True)
+    is_taxcollector = models.BooleanField(default=False)
