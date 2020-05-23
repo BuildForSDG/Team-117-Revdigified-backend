fab68fa2d93ac520df289770fdb281332e51691b
Samizzo <samizzographics@gmail.com>
2020-05-23 17:16:52 +0300

    api

--- api/routing/serializers.py
+++ api/routing/serializers.py
@@ -4,5 +4,5 @@ from rest_framework import serializers
 class RDUserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = RDUser
-        fields = ['url','email','first_name','DOB','is_taxpayer','is_taxcollector','is_auditor','is_systemAdmin','is_staff']
+        fields = ['url','email','DOB','is_taxpayer','is_taxcollector','is_auditor','is_systemAdmin','is_staff']
         
